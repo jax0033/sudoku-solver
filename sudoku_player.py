197c22a509637eefad2b0be27d963eddf521832a
jax0033 <jax0033@protonmail.com>
2020-07-01 16:42:20 +0200

    Update sudoku_player.py
    Cleaned up useless function and messy code, added comments for description

--- sudoku_player.py
+++ sudoku_player.py
@@ -87,21 +87,6 @@ def is_possible(x,y,num):
 			return True
 	return False
 
-#returns all possible numbers at (x,y)
-def possible_nums(x,y):
-	possible = []
-	if grid[y][x] == 0:
-		ongrid = allnums(x,y)
-		lst = [i for i in range(1,10)]
-		for el in lst:
-			if el not in ongrid:
-				possible.append(el)
-		print(possible)
-		if len(possible) == 0:
-			return False
-		return possible
-	return False
-
 #solves the grid using backtracking and recursion
 #this is, by far, the worst mess i have encountered yet. It does work..
 def solve(sgrid,temp):
@@ -119,11 +104,6 @@ def solve(sgrid,temp):
 				return
 	temp3.append(np.matrix(sgrid))
 
-
-#solve(grid)
-
-
-
 def solve_anim(sgrid):
 	global timer_d0c
 	screen.fill((255,255,255))
@@ -140,9 +120,6 @@ def solve_anim(sgrid):
 				return
 	input("Different one?")
 
-
-
-
 def drawgrid():
 	for n in range(1,3):
 		pygame.draw.line(screen,(0,0,0),(n*(width/3),0),(n*(width/3),heigth),5)
@@ -150,12 +127,6 @@ def drawgrid():
 	for n in range(1,10):
 		pygame.draw.line(screen,(0,0,0),(n*(width/9),0),(n*(width/9),heigth),1)
 		pygame.draw.line(screen,(0,0,0),(0,n*(heigth/9)),(width,n*(heigth/9)),1)
-
-def drawboard(board,font):
-	for x in range(9):
-		for y in range(9):
-			if board[y][x] != 0:
-				draw_text(board[y][x],font,x,y)
 
 def mod_draw_board(board):
 	for event in pygame.event.get():
@@ -175,16 +146,9 @@ def mod_draw_board(board):
 		for y in range(9):
 			if temp_47e[c] != 0:
 				draw_text(temp_47e[c],font,y,x)
-				#time.sleep()
+				time.sleep(0.003)
 			c+=1
 	pygame.display.update()
-
-
-
-
-
-def bigtile(x,y):
-	pygame.draw.rect(screen,(255,0,0),(x,y,299,299),3)
 
 def smalltile(rect):
 	pygame.draw.rect(screen,(255,0,0),rect,3)
@@ -194,70 +158,76 @@ def draw_text(text,font,x,y):
 	textobj = font.render(text,1,(0,0,0))
 	screen.blit(textobj,pygame.Rect(x*100+30,y*100+30,100,100))
 
-
 def main():
+	
+	#temp3 stores the finished sudoku
 	global temp3
 	font = pygame.font.SysFont(None,60)
 	pygame.display.set_caption("Sudoku")
 	
-	n = 0
+	#contains information about the <Rect> obj, position, if it is mutable and the displayed number. None if None
 	rectangles = []
-	used_tiles = []
-	board = []
-	temp_b09 = []
+
 	
+	"""
+	sorts given sudoku !!ONLY FIRST POSSIBLE SOLUTION!! and converts it into array
+	first solution meaning the one that uses lower values from top to bottom from left to right
+	"""
 	solve(grid,[])
 	temp3 = str(temp3[0])
-	
+	temp_b09 = []
+
 	for s in temp3:
 		if s.isdigit():
 			temp_b09.append(int(s))
 	
-	#constructing necessary board/controls
+	#all tile information (hitbox,pos,immutability,number)
+	n = 0
 	for y in range(9):
 		for x in range(9):
-			rectangles.append([pygame.Rect(x*100,y*100,100,100),(x,y),False,None],)
+			rectangles.append([pygame.Rect(x*100,y*100,100,100),(x,y),False,None])
 
 			if grid[y][x] != 0:
 				rectangles[n] = [pygame.Rect(x*100,y*100,100,100),(x,y),True,grid[y][x]]
-				board.append([grid[y][x],(x,y),True])
-				used_tiles.append((x,y))
 			n+=1
 
-	initialize_skynet_algorithm = False
+	#bools for controlling loops and selection
+	solve_sudoku = False
 	selection = False
-	key = None
-	selector = None
-	fps = 30
-	clock = pygame.time.Clock()
-	having_fun_solving_the_sudoku = True
-	while having_fun_solving_the_sudoku:
+	running = True
 
-		if initialize_skynet_algorithm:
+	selector = None
+	clock = pygame.time.Clock()
+	fps = 30
+	while running:
+
+		if solve_sudoku:
 			solve_anim(grid)
 			l = 0
 			for x in range(9):
 				for y in range(9):
 					draw_text(temp_b09[l],font,y,x)
 					pygame.display.update()
-
 					l += 1
-			time.sleep(20)
-			having_fun_solving_the_sudoku = False
+			running = False
+
 		else:
+
 			click = False
 			key = None
 			screen.fill((255,255,255))
 			drawgrid()
-			
+
 			mx,my = pygame.mouse.get_pos()
-			
+
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
+
 				if event.type == pygame.MOUSEBUTTONDOWN:
 					if event.button == 1:
 						click = True
+
 				if event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_1:
 						key = 1
@@ -277,9 +247,8 @@ def main():
 						key = 8
 					if event.key == pygame.K_9:
 						key = 9
-
 					if event.key == pygame.K_SPACE:
-						initialize_skynet_algorithm = True
+						solve_sudoku = True
 
 			for rect in rectangles:
 				if rect[0].collidepoint((mx,my)):
@@ -287,9 +256,10 @@ def main():
 						selector = rect
 						selection = True
 
+			#if a tile is selected:
 			if selection:
+				#visually displays what tile is selected
 				smalltile(selector[0])
-				temp = True
 				for tile in rectangles:
 					if selector[1] == tile[1] and tile[2] is not True and key is not None:
 						for n,all_ in enumerate(rectangles):
@@ -297,7 +267,7 @@ def main():
 								rectangles[n][-1] = key
 								key = None
 
-
+			#clears ugrid
 			ugrid = []
 			for tile in rectangles:
 				if tile[-1] == None:
@@ -307,10 +277,14 @@ def main():
 				if tile[-1] != None:
 					draw_text(tile[-1],font,tile[1][0],tile[1][1])
 			if temp_b09 == ugrid:
-				having_fun_solving_the_sudoku = False
+				running = False
 
 		clock.tick(fps)
 		pygame.display.update()
 
 main()
 print("finished")
+while true:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
