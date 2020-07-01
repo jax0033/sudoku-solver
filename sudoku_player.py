4f48c2749e19128c8e067f90928656b80b274e65
jax0033 <jax0033@protonmail.com>
2020-07-01 11:42:25 +0200

    Update sudoku_player.py
    You can now press space to see the algorithm work live !AND! flip the entire board horizontally and vertically!! isn't that great?

--- sudoku_player.py
+++ sudoku_player.py
@@ -1,6 +1,7 @@
 import os
 import pygame
 import numpy as np
+import time
 
 """
 import requests
@@ -13,6 +14,9 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = "400,75"
 pygame.init()
 screen = pygame.display.set_mode((900,900))
 width,heigth = 900,900
+
+font = pygame.font.SysFont(None,60)
+
 #2dimensional array
 grid = [[5,1,2,4,0,9,0,6,3],
 		[0,6,0,0,2,3,0,9,4],
@@ -96,22 +100,41 @@ def possible_nums(x,y):
 	return False
 
 #solves the grid using backtracking and recursion
-def solve(sgrid):
+#this is, by far, the worst mess i have encountered yet. It does work..
+def solve(sgrid,temp):
+	global temp3
+	for x in range(9):
+		for y in range(9):
+			if sgrid[y][x] == 0:
+				for n in range(1,10):
+					if is_possible(x,y,n):
+						temp3 = temp
+						sgrid[y][x] = n
+						solve(sgrid,temp)
+						sgrid[y][x] = 0
+				#if the program cant recurse anymore it means the sudoku is finished
+				return
+	temp3.append(np.matrix(sgrid))
+
+
+#solve(grid)
+
+
+
+def solve_anim(sgrid):
 	for x in range(9):
 		for y in range(9):
 			if sgrid[y][x] == 0:
 				for n in range(1,10):
 					if is_possible(x,y,n):
 						sgrid[y][x] = n
-						solve(sgrid)
+						mod_draw_board(np.matrix(sgrid))
+						solve_anim(sgrid)
 						sgrid[y][x] = 0
-
-				#if the program cant recurse anymore it means the sudoku is finished
 				return
-	#ürints the result(s) in a readable format
-	print(np.matrix(sgrid))
 
-#solve(grid)
+
+
 
 def drawgrid():
 	for n in range(1,3):
@@ -121,8 +144,35 @@ def drawgrid():
 		pygame.draw.line(screen,(0,0,0),(n*(width/9),0),(n*(width/9),heigth),1)
 		pygame.draw.line(screen,(0,0,0),(0,n*(heigth/9)),(width,n*(heigth/9)),1)
 
-def drawboard():
-	pass
+def drawboard(board,font):
+	for x in range(9):
+		for y in range(9):
+			if board[y][x] != 0:
+				draw_text(board[y][x],font,x,y)
+
+def mod_draw_board(board):
+	screen.fill((255,255,255))
+	drawgrid()
+	print(board)
+	kurwa = []
+	board = str(board)
+
+	for s in board:
+		if s.isdigit():
+			kurwa.append(int(s))
+	print(f"kurwa {kurwa}")
+	c = 0
+	for x in range(9):
+		for y in range(9):
+			if kurwa[c] != 0:
+				draw_text(kurwa[c],font,x,y)
+				time.sleep(0.02)
+			c+=1
+	pygame.display.update()
+
+
+
+
 
 def bigtile(x,y):
 	pygame.draw.rect(screen,(255,0,0),(x,y,299,299),3)
@@ -133,17 +183,29 @@ def smalltile(rect):
 def draw_text(text,font,x,y):
 	text = str(text)
 	textobj = font.render(text,1,(0,0,0))
-	screen.blit(textobj,pygame.Rect(x*100+41,y*100+41,100,100))
+	screen.blit(textobj,pygame.Rect(x*100+30,y*100+30,100,100))
 
-solved_grid = solve(grid)
 
 def main():
-	font = pygame.font.SysFont(None,32)
+	global temp3
+	font = pygame.font.SysFont(None,60)
+	pygame.display.set_caption("Sudoku")
 	rectangles = []
+	initialize_skynet_algorithm = False
 	board = []
 	key = None
 	used_tiles = []
 	n = 0
+	kurwa = []
+	solve(grid,[])
+	#print(temp3[0])
+	temp3 = str(temp3[0])
+	for s in temp3:
+		if s.isdigit():
+			kurwa.append(int(s))
+	#print(kurwa)
+	board_raw = []
+	#constructing necessary board/controls
 	for y in range(9):
 		for x in range(9):
 			rectangles.append([pygame.Rect(x*100,y*100,100,100),(x,y),False,None],)
@@ -152,73 +214,94 @@ def main():
 				rectangles[n] = [pygame.Rect(x*100,y*100,100,100),(x,y),True,grid[y][x]]
 				board.append([grid[y][x],(x,y),True])
 				used_tiles.append((x,y))
+				board_raw.append(grid[y][x])
 			n+=1
 
 	selector = None
 	selection = False
 	fps = 30
 	clock = pygame.time.Clock()
-	
-	while True:
-		click = False
-		key = None
-		screen.fill((255,255,255))
-		drawgrid()
-		
-		mx,my = pygame.mouse.get_pos()
-		
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				if event.button == 1:
-					click = True
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_1:
-					key = 1
-				if event.key == pygame.K_2:
-					key = 2
-				if event.key == pygame.K_3:
-					key = 3
-				if event.key == pygame.K_4:
-					key = 4
-				if event.key == pygame.K_5:
-					key = 5
-				if event.key == pygame.K_6:
-					key = 6
-				if event.key == pygame.K_7:
-					key = 7
-				if event.key == pygame.K_8:
-					key = 8
-				if event.key == pygame.K_9:
-					key = 9
+	having_fun_solving_the_sudoku = True
+	while having_fun_solving_the_sudoku:
 
-		for rect in rectangles:
-			if rect[0].collidepoint((mx,my)):
-				if click:
-					selector = rect
-					selection = True
+		if initialize_skynet_algorithm:
+			solve_anim(grid)
+			l = 0
+			for x in range(9):
+				for y in range(9):
+					draw_text(kurwa[l],font,x,y)
+					pygame.display.update()
 
-		if selection:
-			smalltile(selector[0])
-			temp = True
-			for tile in rectangles:
-				if selector[1] == tile[1] and tile[2] is not True and key is not None:
-					for n,all_ in enumerate(rectangles):
-						if all_[1] == selector[1]:
-							rectangles[n][-1] = key
-							key = None
-
-
-		for tile in rectangles:
-			if tile[-1] != None:
-				draw_text(tile[-1],font,tile[1][0],tile[1][1])
+					l += 1
+			time.sleep(20)
+			having_fun_solving_the_sudoku = False
+		else:
+			click = False
+			key = None
+			screen.fill((255,255,255))
+			drawgrid()
 			
+			mx,my = pygame.mouse.get_pos()
+			
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+				if event.type == pygame.MOUSEBUTTONDOWN:
+					if event.button == 1:
+						click = True
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_1:
+						key = 1
+					if event.key == pygame.K_2:
+						key = 2
+					if event.key == pygame.K_3:
+						key = 3
+					if event.key == pygame.K_4:
+						key = 4
+					if event.key == pygame.K_5:
+						key = 5
+					if event.key == pygame.K_6:
+						key = 6
+					if event.key == pygame.K_7:
+						key = 7
+					if event.key == pygame.K_8:
+						key = 8
+					if event.key == pygame.K_9:
+						key = 9
+
+					if event.key == pygame.K_SPACE:
+						initialize_skynet_algorithm = True
+
+			for rect in rectangles:
+				if rect[0].collidepoint((mx,my)):
+					if click:
+						selector = rect
+						selection = True
+
+			if selection:
+				smalltile(selector[0])
+				temp = True
+				for tile in rectangles:
+					if selector[1] == tile[1] and tile[2] is not True and key is not None:
+						for n,all_ in enumerate(rectangles):
+							if all_[1] == selector[1]:
+								rectangles[n][-1] = key
+								key = None
+
+
+			ugrid = []
+			for tile in rectangles:
+				if tile[-1] == None:
+					ugrid.append(0)
+				else:
+					ugrid.append(tile[-1])
+				if tile[-1] != None:
+					draw_text(tile[-1],font,tile[1][0],tile[1][1])
+			if kurwa == ugrid:
+				having_fun_solving_the_sudoku = False
+
 		clock.tick(fps)
 		pygame.display.update()
 
-
 main()
-
-
-
+print("finished")
