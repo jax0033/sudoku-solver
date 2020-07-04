723f732b2aabe48258e1303d5a06a3ecc1709ebe
jax0033 <jax0033@protonmail.com>
2020-07-04 04:23:14 +0200

    Update getsudoku.py
    Uses the cheat sheet from the website itself,which is found right below the board.

--- getsudoku.py
+++ getsudoku.py
@@ -5,23 +5,19 @@ def getsudoku():
 	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}
 	result = requests.get(url, headers=headers).content.decode()
 
-	result = result.split("SIZE=2 AUTOCOMPLETE=off NAME=")
-	temp = ""
-	for s in result:
-		temp += s + ""
-	temp = temp.split(" ")
-
+	result = result.split('"')
 	temp_0dc = []
-	for n,s in enumerate(temp):
-		if s.upper().startswith("MAXLENGTH") or s.upper().startswith("READONLY"):
-			temp_0dc.append([temp[n],temp[n+1]])
+	for e in result:
+		if e.isdigit() and len(e) == 81:
+			temp_0dc.append(e)
 
 	grid = [[0 for i in range(9)] for i in range(9)]
 	k = 0
 	for x in range(9):
 		for y in range(9):
-			if temp_0dc[k][0].upper() == "READONLY":
-				grid[x][y] = int(temp_0dc[k][1][-2])
+			if temp_0dc[1][k] == "1":
+
+				grid[x][y] = int(temp_0dc[0][k])
 			else:
 				grid[x][y] = 0
 			k+=1
